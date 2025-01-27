e2ac31ca9190caec4735616bd38a40cfa344c683
CarlosGuilherme <carlosguilherme.schneider@gmail.com>
2025-01-26 23:48:25 -0300

    commit inicial

---
+++ src/main/java/fallback/FallbackCalculadora.java
@@ -0,0 +1,9 @@
+package fallback;
+
+public class FallbackCalculadora {
+
+    public String mensagemFallback() {
+
+        return "Desculpe estamos em obras no momento, por favor tente mais tarde.";
+    }
+}
